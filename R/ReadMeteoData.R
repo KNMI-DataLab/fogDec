10ee7defd10dc0cacfb61cbed88e1dae8c31a866
andreapago <andrea.pago@gmail.com>
2016-10-14 18:56:42 +0200

    Added the wind feature in the decision tree model. Add ReadMeteo to read in meteo data (only wind at the moment).

---
+++ R/ReadMeteoData.R
@@ -0,0 +1,29 @@
+#' Read wind data files
+#' @param filenames List of filenames
+#' @return data.table
+#' @export
+ReadWindData <- function(filenames) {
+  dateTime <- day <- IT_DATETIME <- TOW.FF_10M <- NULL
+  sensorData <- rbindlist(lapply(filenames, read.csv, stringsAsFactors = FALSE))
+  sensorData <- data.table(sensorData)
+  sensorData[TOW.FF_10M  == '', TOW.FF_10M  := NA]
+  #sensorData[, hhmmss := CorrectOurs(hhmmss)]
+  sensorData[, IT_DATETIME := as.POSIXct(sensorData[, IT_DATETIME], format = "%Y%m%d_%H%M%S", tz = "UTC")]
+  setnames(sensorData, "IT_DATETIME", "dateTime")
+  #sensorData[, hhmmss := NULL]
+  sensorData[, year   := year(dateTime)]
+  sensorData[, month  := month(dateTime)]
+  sensorData[, day    := mday(dateTime)]
+  sensorData[, hour   := hour(dateTime)]
+  setcolorder(sensorData, c(2, 1, 5, 6, 7, 8, 3, 4))
+  return(sensorData)
+}
+
+
+##THIS HAS TO BE TESTED##
+SynchronizeSensorReadingsNoMORPicture <- function(sensorDataDT, imageInfoDT){
+setkey(sensorDataDT, dateTime)
+setkey(imageInfoDT, dateTime)
+sensorReadingsNoMOR <- sensorData[imageInfoDT, roll = "nearest"]
+sensorReadingsNoMOR
+}
