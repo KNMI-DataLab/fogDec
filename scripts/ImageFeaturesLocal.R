6511b355aa1da453a09dfb5c6f345662b5335f45
MartinRoth <marsianus@gmail.com>
2016-12-02 18:57:16 +0100

    List directories

--- scripts/ImageFeaturesLocal.R
+++ scripts/ImageFeaturesLocal.R
@@ -10,42 +10,55 @@ library(maptools)
 
 registerDoParallel(cores=2)
 
-filenames <- list.files("/net/bhw420/nobackup/users/haijde/DATA/AXIS214/Meetterrein",
-                        recursive = TRUE,
-                        pattern=glob2rx("Meetterrein_*.jpg"),
-              ful
+basePath <- "/net/bhw420/nobackup/users/haijde/DATA/AXIS214/Meetterrein"
+# basePath <- "~/Academia/"
+# output   <- "/nobackup/users/roth/processedImages/Meetterrein"
 
-properties <- fread(system.file("extdata/properties.csv", package="visDec"))
+directories <- dir(basePath,
+                   pattern = glob2rx("*nmi*"))
 
-path <- system.file("extdata/Meetterrein", package="visDec")
-filenames <- list.files(path,
-                        pattern=glob2rx("Meetterrein_201510*.jpg"),
-                        full.names=TRUE)
-
-imageSummary <- foreach(file = iter(filenames), .combine = rbind) %dopar% {
-  FileNameParser(file, "na*me_yyyymmdd_hhmm.jpg")
+foreach(directory = iter(directories), .combine = "rbind") %do% {
+  message(paste0("Directory ", directory, " is being processed."))
+  filenames <- list.files(paste0(basePath, directory),
+                          pattern=glob2rx("Meetterrein_*.jpg"),
+                          full.names = TRUE)
 }
 
+# filenames <- list.files(paste0(basePath, directory),
+#                         pattern=glob2rx("Meetterrein_*.jpg"),
+#                         full.names = TRUE)
 
-setkey(imageSummary, filePrefix, dateTime)
-
-imageSummary <- merge(imageSummary, properties,
-                      by.x = "filePrefix", by.y = "filePrefix")
-
-daylightImages <- imageSummary[IsDayLightImage(dateTime, lon, lat), ]
-
-
-invisible(daylightImages[, id := 1:.N])
-setkey(daylightImages, id)
-
-imageFeatures <- foreach(id = iter(daylightImages[, id]), .combine = rbind) %dopar% {
-  daylightImages[id, ImageFeatures(filePath, y > 16)]
-}
-
-setkey(daylightImages, filePath)
-setkey(imageFeatures, filePath)
-
-imageSummary <- merge(daylightImages, imageFeatures)
+# properties <- fread(system.file("extdata/properties.csv", package="visDec"))
+# 
+# path <- system.file("extdata/Meetterrein", package="visDec")
+# filenames <- list.files(path,
+#                         pattern=glob2rx("Meetterrein_201510*.jpg"),
+#                         full.names=TRUE)
+# 
+# imageSummary <- foreach(file = iter(filenames), .combine = rbind) %dopar% {
+#   FileNameParser(file, "na*me_yyyymmdd_hhmm.jpg")
+# }
+# 
+# 
+# setkey(imageSummary, filePrefix, dateTime)
+# 
+# imageSummary <- merge(imageSummary, properties,
+#                       by.x = "filePrefix", by.y = "filePrefix")
+# 
+# daylightImages <- imageSummary[IsDayLightImage(dateTime, lon, lat), ]
+# 
+# 
+# invisible(daylightImages[, id := 1:.N])
+# setkey(daylightImages, id)
+# 
+# imageFeatures <- foreach(id = iter(daylightImages[, id]), .combine = rbind) %dopar% {
+#   daylightImages[id, ImageFeatures(filePath, y > 16)]
+# }
+# 
+# setkey(daylightImages, filePath)
+# setkey(imageFeatures, filePath)
+# 
+# imageSummary <- merge(daylightImages, imageFeatures)
 
 ##stop the cluster
 stopImplicitCluster()
