fdd9c6d23d3186229718c206c6adfdfd28345998
MartinRoth <marsianus@gmail.com>
2016-12-02 18:18:47 +0100

    Add local feature extraction script

---
+++ scripts/ImageFeaturesLocal.R
@@ -0,0 +1,57 @@
+## script: compute image features
+
+## Load Libraries
+library(data.table)
+library(fogDec)
+library(visDec)
+library(ggplot2)
+library(doParallel)
+library(maptools)
+
+registerDoParallel(cores=2)
+
+
+properties <- fread(system.file("extdata/properties.csv", package="visDec"))
+
+path <- system.file("extdata/Meetterrein", package="visDec")
+filenames <- list.files(path,
+                        pattern=glob2rx("Meetterrein_201510*.jpg"),
+                        full.names=TRUE)
+
+imageSummary <- foreach(file = iter(filenames), .combine = rbind) %dopar% {
+  FileNameParser(file, "na*me_yyyymmdd_hhmm.jpg")
+}
+
+
+setkey(imageSummary, filePrefix, dateTime)
+
+imageSummary <- merge(imageSummary, properties,
+                      by.x = "filePrefix", by.y = "filePrefix")
+
+daylightImages <- imageSummary[IsDayLightImage(dateTime, lon, lat), ]
+
+
+invisible(daylightImages[, id := 1:.N])
+setkey(daylightImages, id)
+
+imageFeatures <- foreach(id = iter(daylightImages[, id]), .combine = rbind) %dopar% {
+  daylightImages[id, ImageFeatures(filePath, y > 16)]
+}
+
+setkey(daylightImages, filePath)
+setkey(imageFeatures, filePath)
+
+imageSummary <- merge(daylightImages, imageFeatures)
+
+##stop the cluster
+stopImplicitCluster()
+
+
+
+
+
+
+
+
+
+
