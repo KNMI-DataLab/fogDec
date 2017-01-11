d1da44044b0637c180f867cfd13ff305776b2c5e
MartinRoth <marsianus@gmail.com>
2017-01-11 12:20:38 +0100

    Removed spaces in file names (non portability issue)

--- scripts/mokkupMap.R
+++ scripts/mokkupMap.R
@@ -1,8 +1,8 @@
 
 library(rgdal)
 
-shp = "inst/extdata/Snelwegen/NDWApril2016WGS84/NDW April 2016 WGS84.shp"
-tmp <- readOGR(shp, layer = "NDW April 2016 WGS84")
+shp = "inst/extdata/Snelwegen/NDWApril2016WGS84/NDW_April_2016_WGS84.shp"
+tmp <- readOGR(shp, layer = "NDW_April_2016_WGS84")
 
 wegnummer <- c("001", "004", "006", "007", "009",
                "012", "015",
