7f077e0730a6681b05c0580842e9de7dfca54893
andreapago <andrea.pago@gmail.com>
2017-03-22 16:36:33 +0000

    images and image features stores in DB for Cabauw and De Bilt.

--- scripts/insertInPostgresDB.R
+++ scripts/insertInPostgresDB.R
@@ -83,3 +83,95 @@ dbWriteTable(con, "image_features", tmp, append = TRUE, row.names = FALSE, match
 allfeatures <- as.data.table(dbReadTable(con, "image_features"))
 
 tmp <- as.data.table(dbGetQuery(con, "SELECT * FROM image_features WHERE extract(year from timestamp) = 2016 AND extract(month from timestamp) = 10 AND extract(day from timestamp) = 10;"))
+
+
+
+
+
+
+
+
+##########################De Bilt images######################################################################
+filesDeBilt <- list.files("/net/pc150395/nobackup/users/roth/processedImages/Meetterrein",full.names = T)
+listDFSummary <- lapply(filesDeBilt, readRDS)
+deBiltDataSummary <- rbindlist(listDFSummary)
+Sys.setenv(TZ = "UTC")
+tmp <- deBiltDataSummary[,.(camera_id=1, timestamp = dateTime, filepath = filePath, day_phase = 1)]
+
+dbListFields(con, "images")
+Sys.setenv(TZ = "UTC")
+dbReadTable(con, "images")
+
+dbWriteTable(con, "images", tmp, append = TRUE, row.names = FALSE, match.cols = TRUE)
+##############################################################################################################
+
+
+###################Cabauw images##############################################################################
+## Images
+dbListFields(con, "images")
+Sys.setenv(TZ = "UTC")
+dbReadTable(con, "images")
+
+filesCabauw <- list.files("/net/pc150395/nobackup/users/roth/processedImages/Cabauw", full.names = T)
+listDFSummary <- lapply(filesCabauw, readRDS)
+cabauwDataSummary <- rbindlist(listDFSummary)
+
+tmp <- cabauwDataSummary[locationID == "NL4", .(camera_id=3, timestamp = dateTime, filepath = filePath, day_phase = 1)]
+dbWriteTable(con, "images", tmp, append = TRUE, row.names = FALSE, match.cols = TRUE)
+
+tmp2 <- cabauwDataSummary[locationID == "NL3", .(camera_id=2, timestamp = dateTime, filepath = filePath, day_phase = 1)]
+dbWriteTable(con, "images", tmp2, append = TRUE, row.names = FALSE, match.cols = TRUE)
+##############################################################################################################
+
+
+
+###Image features De Bilt#####################################################################################
+filesDeBilt <- list.files("/net/pc150395/nobackup/users/roth/processedImages/Meetterrein", full.names = T)
+listDFSummary <- lapply(filesDeBilt, readRDS)
+deBiltDataSummary <- rbindlist(listDFSummary)
+
+
+dbListFields(con, "image_features")
+dbReadTable(con, "image_features")
+
+images <- as.data.table(dbReadTable(con, "images"))
+fullSummary <- merge(deBiltDataSummary, images, by.x = "filePath", by.y="filepath")
+
+tmp <- fullSummary[, .(image_id, camera_id, timestamp, mean_edge = meanEdge,
+                       change_point = changePoint, smoothness,
+                       fractal_dim = fractalDim, mean_hue = meanHue, 
+                       mean_saturation = meanSaturation,
+                       mean_brightness = meanBrightness)]
+
+dbWriteTable(con, "image_features", tmp, append = TRUE, row.names = FALSE, match.cols = TRUE)
+##############################################################################################################
+
+
+###Image features Cabauw######################################################################################
+filesCabauw<-list.files("/net/pc150395/nobackup/users/roth/processedImages/Cabauw", full.names = T)
+listDFSummary<-lapply(filesCabauw, readRDS)
+cabauwDataSummary<-rbindlist(listDFSummary)
+
+
+dbListFields(con, "image_features")
+dbReadTable(con, "image_features")
+
+images <- as.data.table(dbReadTable(con, "images"))
+fullSummary <- merge(cabauwDataSummary, images, by.x = "filePath", by.y="filepath")
+
+tmp <- fullSummary[, .(image_id, camera_id, timestamp, mean_edge = meanEdge,
+                       change_point = changePoint, smoothness,
+                       fractal_dim = fractalDim, mean_hue = meanHue, 
+                       mean_saturation = meanSaturation,
+                       mean_brightness = meanBrightness)]
+
+dbWriteTable(con, "image_features", tmp, append = TRUE, row.names = FALSE, match.cols = TRUE)
+##############################################################################################################
+imageFeatures <- as.data.table(dbReadTable(con, "image_features"))
+
+
+
+
+
+
+
