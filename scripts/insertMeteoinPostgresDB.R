560429b5d0c7607ed91b69b5b73d5dffba3c4ad4
andreapago <andrea.pago@gmail.com>
2017-07-12 12:55:08 +0000

    maybe a solution, but probably some more general solution done at the knmiR side

--- scripts/insertMeteoinPostgresDB.R
+++ scripts/insertMeteoinPostgresDB.R
@@ -10,14 +10,27 @@ library(knmiR)
 
 
 
-getTemp<-function(x){
-  value<-KIS(var = "MOR_10", '260_A_a', as.character(x))
+getTemp<-function(x, location){
+  if(location==1){
+  value<-KIS(var = "MOR_10", '260_A_a', as.character(x)) #DeBilt
+  }
+  if(location==3){
+  value<-KIS(var = "MOR_10", '348_A_a', as.character(x))#Cabauw
+  }
+  if(location==7){
+    value<-KIS(var = "MOR_10", '280_A_23t', as.character(x))#Eelde
+    
+  }
+  #if(location==3){
+   # value<-KIS(var = "MOR_10", '348_A_a', as.character(x))
+    
+  #}
   value
 }
 
 
 
-
+prepareMeteoTable<-function(location){
 
 
 dbConfig <- fromJSON("config.json")
@@ -63,7 +76,7 @@ temp2<-tempTB[meteoFeaturesTable]
 
 dataNoMeteoAll<-tempTB[!(which(tempTB$image_id %in% temp2$image_id))]
 
-dataNoMeteoDebilt<-dataNoMeteoAll[location_id == 1, location_id,timestamp]
+dataNoMeteoDebilt<-dataNoMeteoAll[location_id == location, location_id,timestamp]
 
 datesRequired<-unique(as.Date(dataNoMeteoDebilt[,timestamp]))
 
@@ -73,7 +86,7 @@ today<-as.Date(Sys.time())
 
 datesToFetch<-datesRequired[datesRequired != today]
 
-values<-lapply(datesToFetch, getTemp)
+values<-lapply(datesToFetch, getTemp, location)
 values<-rbindlist(values)
 
 
@@ -84,7 +97,9 @@ values[, IT_DATETIME := as.POSIXct(values[, IT_DATETIME], format = "%Y%m%d_%H%M%
 setnames(values, "IT_DATETIME", "timestamp")
 tmp <- within(values, rm(DS_CODE, "TOA.Q_MOR_10"))
 setnames(tmp,"TOA.MOR_10" ,"mor_visibility")
-tmp[,location_id:=1]#de bilt
+tmp[,location_id:=location]
+}
+
 
 dbWriteTable(con, "meteo_features_stations", tmp, append = TRUE, row.names = FALSE, match.cols = TRUE)
 
