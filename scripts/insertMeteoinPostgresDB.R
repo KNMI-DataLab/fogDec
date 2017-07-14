5b6d7db63e73511c3d686761a7998074d5fe6038
andreapago <andrea.pago@gmail.com>
2017-07-14 14:57:46 +0000

    added stuff for adding meteo columns not yet fetched

--- scripts/insertMeteoinPostgresDB.R
+++ scripts/insertMeteoinPostgresDB.R
@@ -2,33 +2,55 @@ library(DBI)
 library(jsonlite)
 library(data.table)
 library(knmiR)
+library(postGIStools)
 
-
-getValueFromKIS<-function(x, location){
+getValueFromKIS<-function(x, location, variable){
+  if(variable == "mor_visibility"){
+    varForKIS <- "MOR_10"
+    sensorType <- "A"
+  }
+  if(variable == "wind_speed"){
+    varForKIS <- "FF_10M_10"
+    sensorType <- "W"
+  }
+  if(variable == "air_temp"){
+    varForKIS <- "T_DRYB_10"
+    sensorType <- "T"
+  }
+  if(variable == "dew_point"){
+    varForKIS <- "T_DEWP_10"
+    sensorType <- "T"
+  }
+  if(variable == "rel_humidity"){
+    varForKIS <- "U_10"
+    sensorType <- "T"
+  }
+  
+  
   if(location==1){
-  value<-KIS(var = "MOR_10", '260_A_a', as.character(x)) #DeBilt
+  value<-KIS(var = varForKIS, paste0("260_",sensorType,"_a"), as.character(x)) #DeBilt
   }
   if(location==3){
-  value<-KIS(var = "MOR_10", '348_A_a', as.character(x))#Cabauw
+  value<-KIS(var = varForKIS, paste0("348_",sensorType,"_a"), as.character(x))#Cabauw
   }
   if(location==6){
-    value<-KIS(var = "MOR_10", '380_A_22t', as.character(x))#Beek
+    value<-KIS(var = varForKIS, paste0("380_",sensorType,"_22t"), as.character(x))#Beek
   }
   if(location==7){
-    value<-KIS(var = "MOR_10", '280_A_23t', as.character(x))#Eelde
+    value<-KIS(var = varForKIS, paste0("280_",sensorType,"_23t"), as.character(x))#Eelde
   }
   if(location==8){
-   value<-KIS(var = "MOR_10", '344_A_24t', as.character(x))#Rotterdam Airport
+   value<-KIS(var = varForKIS, paste0("344_",sensorType,"_24t"), as.character(x))#Rotterdam Airport
   }
   if(location==9){
-    value<-KIS(var = "MOR_10", '240_A_18Ct', as.character(x))#Schiphol Airport
+    value<-KIS(var = varForKIS, paste0("240_",sensorType,"_18Ct"), as.character(x))#Schiphol Airport
   }
   value
 }
 
 
 
-prepareMeteoTable<-function(location){
+prepareMeteoTable<-function(location, variable,newval){
 
 
 dbConfig <- fromJSON("config.json")
@@ -56,37 +78,155 @@ setkey(tempTB,timestamp,location_id)
 setkey(meteoFeaturesTable, timestamp,location_id)
 temp2<-tempTB[meteoFeaturesTable]
 
-dataNoMeteoAll<-tempTB[!(which(tempTB$image_id %in% temp2$image_id))]
+
+
+
+
+
+
+if(newval==TRUE){
+
+dataNoMeteoAll<-tempTB[!(which(tempTB$image_id %in% temp2$image_id))]#images have no meteo fetures at all
 dataNoMeteo<-dataNoMeteoAll[location_id == location, location_id,timestamp]
 dataNoMeteo<-dataNoMeteo[,unique(dataNoMeteo)]
 datesRequired<-unique(as.Date(dataNoMeteo[,timestamp]))
+}
+else{
+  test<-meteoFeaturesTable[location_id==location & is.na(variable),location_id,timestamp]
+  datesRequired<-unique(as.Date(test[,timestamp]))
+
+  #values<-lapply(datesRequired, getValueFromKIS, 1, variable)
+  #tmp <- within(values, rm(DS_CODE, "TOW.Q_FF_10M_10"))
+  
+  
+ 
+  
+  
+}
 
 today<-as.Date(Sys.time())
 datesToFetch<-datesRequired[datesRequired != today]
-values<-lapply(datesToFetch, getValueFromKIS, location)
+values<-lapply(datesToFetch, getValueFromKIS, location, variable)
 values<-rbindlist(values)
-values[TOA.MOR_10  == -1, TOA.MOR_10  := NA]
-#sensorData[, hhmmss := CorrectOurs(hhmmss)]
+
+
 values[, IT_DATETIME := as.POSIXct(values[, IT_DATETIME], format = "%Y%m%d_%H%M%S", tz = "UTC")]
 setnames(values, "IT_DATETIME", "timestamp")
+
+if(variable=="visibility"){
+values[TOA.MOR_10  == -1, TOA.MOR_10  := NA]
 tmp <- within(values, rm(DS_CODE, "TOA.Q_MOR_10"))
 setnames(tmp,"TOA.MOR_10" ,"mor_visibility")
-tmp[,location_id:=location]
-#check to put just the missing time stamps otherwise getting exception in the DB if trying to fill meteo for timestamps already present
-toBeFilled<-tmp[which(tmp$timestamp %in% dataNoMeteo$timestamp)]
+}
+
+if(variable=="windSpeed"){
+  ##values[TOW.FF_10M_10  == -1, TOW.FF_10M_10  := NA]
+  tmp <- within(values, rm(DS_CODE, "TOW.Q_FF_10M_10"))
+  setnames(tmp,"TOW.FF_10M_10" ,"wind_speed")
+}
+
+if(variable=="humidity"){
+  ##values[TOT.U_10  == -1, TOT.U_10  := NA] ##TO BE ADDED WHEN INFO ARE PROVIDED
+  tmp <- within(values, rm(DS_CODE, "TOT.Q_U_10"))
+  setnames(tmp,"TOT.U_10" ,"rel_humidity")
+}
+
+if(variable=="airTemp"){
+  ##values[TOT.T_DRYB_10  == -1, TOT.T_DRYB_10  := NA] ##TO BE ADDED WHEN INFO ARE PROVIDED
+  tmp <- within(values, rm(DS_CODE, "TOT.Q_T_DRYB_10"))
+  setnames(tmp,"TOT.T_DRYB_10" ,"rel_humidity")
+}
+
+if(variable=="dewPointTemp"){
+  ##values[TOT.T_DEWP_10  == -1, TOT.T_DEWP_10  := NA] ##TO BE ADDED WHEN INFO ARE PROVIDED
+  tmp <- within(values, rm(DS_CODE, "TOT.Q_T_DRYB_10"))
+  setnames(tmp,"TOT.T_DEWP_10" ,"rel_humidity")
+}
+
+
+if(newval==TRUE){
+  tmp[,location_id:=location]
+  #check to put just the missing time stamps otherwise getting exception in the DB if trying to fill meteo for timestamps already present
+  toBeFilled<-tmp[which(tmp$timestamp %in% dataNoMeteo$timestamp)]
+}
+
+else{
+setkey(meteoFeaturesTable, location_id, timestamp)
+setkey(tmp, location_id, timestamp)
+toBeFilled<-tmp[meteoFeaturesTable, nomatch=0]
+toBeFilled<-within(toBeFilled, rm(paste0("i.",variable)))
+}
+
+
+
+
+
 toBeFilled
 }
 
 
-#####################################################################################################
-#dbConfig <- fromJSON("config.json")
 
+
+
+#############UPDATE EXISTING ROWS#############################################
+
+
+
+#postgis_update(con,tmp22,"meteo_features_stations",id_cols = "meteo_feature_id",update_cols = "wind_speed")
+
+
+#########################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#####################################################################################################
+# dbConfig <- fromJSON("config.json")
+#  
 # con <- dbConnect(RPostgreSQL::PostgreSQL(),
-#                  dbname = "FOGDB",
-#                  host = dbConfig[["host"]], port = 9418,
-#                  user = dbConfig[["user"]], password = dbConfig[["pw"]])
-#tmp<-prepareMeteoTable(location = )
+#                   dbname = "FOGDB",
+#                   host = dbConfig[["host"]], port = 9418,
+#                   user = dbConfig[["user"]], password = dbConfig[["pw"]])
+# tmp<-prepareMeteoTable(location = 1, variable = "airTemp" )
 #dbWriteTable(con, "meteo_features_stations", tmp, append = TRUE, row.names = FALSE, match.cols = TRUE)
 #dbDisconnect(con)
 #####################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
