787416e8edc2bc3a05b8b82feac6552214198e77
andreapago <andrea.pago@gmail.com>
2017-08-24 09:35:40 +0000

    NN succesful implementaton of debilt cabauw train-test

--- scripts/NNexample.R
+++ scripts/NNexample.R
@@ -1,6 +1,7 @@
 library(foreach)
 library(doParallel)
 library(imager)
+library(nnet)
 library(data.table)
 
 
@@ -47,27 +48,72 @@ f <- paste('vis_class ~',f)
 f <- as.formula(f)
 
 
-net<-nnet(f,dtMat,size=10, MaxNWts=30000)
+net<-nnet(f,dtMat,size=5, MaxNWts=30000)
 
 
 #nn <- neuralnet::neuralnet(f,dtMat[,c(1:30,2353)],hidden=c(10,10,10),linear.output=FALSE)
 
 
 
+resTest2<-readRDS("~/images/Daniel/Testing/ImageDescription2.rds")
+
+
+filesBase<-resTest2$basename
+setwd("~/images/Daniel/Testing/")
+
+
+matTest<-foreach(i=1:length(filesBase), .combine = rbind) %dopar%{
+  image<-load.image(filesBase[[i]])
+  image<-resize(image,28,28)
+  df<-as.data.frame(image)
+  v<-df$value
+  #mat<-rbind(mat,v)
+  v
+}
+dtMatTest<-data.table(matTest)
+
+dtMatTest[,vis_class:=resTest2$vis_class]
+
+
+
+prdicted<-predict(net,dtMatTest)
+
+dtMatTest[,predictedLabels:=colnames(prdicted)[max.col(prdicted, ties.method = "first")]]
+confusion<-data.table(predicted=dtMatTest$predictedLabels,vis_class=resTest2$vis_class)
+
+table(confusion$predicted,confusion$vis_class)
+
+confusionMatrix(confusion$predicted,confusion$vis_class)
 
 
 
 
 
 
+#all files (not good have to get the daylight images ;-)
+setwd("~/images/RWS")
+
+files<-list.files(".","*.jpg", full.names = T,recursive = T)
+
+
+matRWS<-foreach(i=1:length(files), .combine = rbind) %dopar%{
+  message(files[[i]])
+  image<-load.image(files[[i]])
+  image<-resize(image,28,28)
+  df<-as.data.frame(image)
+  v<-df$value
+  #mat<-rbind(mat,v)
+  v
+}
 
 
 
+predictedRWS<-predict(net,matRWS)
 
+predictedRWS<-data.table(predictedRWS)
 
-
-
-
+predictedRWS[,predictedLabels:=colnames(predictedRWS)[max.col(predictedRWS, ties.method = "first")]]
+predictedRWS[,file:=files]
 
 
 
