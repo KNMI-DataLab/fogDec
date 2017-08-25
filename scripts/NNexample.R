e497ec6779a8fc887aa74fee036d738b0ea8e186
andreapago <andrea.pago@gmail.com>
2017-08-25 08:42:11 +0000

    added blurring

--- scripts/NNexample.R
+++ scripts/NNexample.R
@@ -5,6 +5,7 @@ library(nnet)
 library(data.table)
 library(DBI)
 library(jsonlite)
+library(caret)
 
 
 
@@ -28,6 +29,7 @@ setwd("~/images/Daniel/Training/")
 mat<-foreach(i=1:length(filesBase), .combine = rbind) %dopar%{
   image<-load.image(filesBase[[i]])
   image<-resize(image,28,28)
+  image<-blur_anisotropic(image, amplitude = 15000)
   df<-as.data.frame(image)
   v<-df$value
   #mat<-rbind(mat,v)
@@ -51,7 +53,7 @@ f <- paste('vis_class ~',f)
 f <- as.formula(f)
 
 
-net<-nnet(f,dtMat,size=5, MaxNWts=30000)
+net<-nnet(f,dtMat,size=5, MaxNWts=35000, maxit=800)
 
 
 #nn <- neuralnet::neuralnet(f,dtMat[,c(1:30,2353)],hidden=c(10,10,10),linear.output=FALSE)
@@ -64,15 +66,23 @@ resTest2<-readRDS("~/images/Daniel/Testing/ImageDescription2.rds")
 filesBase<-resTest2$basename
 setwd("~/images/Daniel/Testing/")
 
+cl <- makeCluster(16)
+registerDoParallel(cl)
+
+clusterEvalQ(cl, library("imager"))
+
 
 matTest<-foreach(i=1:length(filesBase), .combine = rbind) %dopar%{
   image<-load.image(filesBase[[i]])
   image<-resize(image,28,28)
+  image<-blur_anisotropic(image, amplitude = 15000)
   df<-as.data.frame(image)
   v<-df$value
   #mat<-rbind(mat,v)
   v
 }
+
+stopCluster(cl)
 dtMatTest<-data.table(matTest)
 
 dtMatTest[,vis_class:=resTest2$vis_class]
@@ -86,7 +96,7 @@ confusion<-data.table(predicted=dtMatTest$predictedLabels,vis_class=resTest2$vis
 
 table(confusion$predicted,confusion$vis_class)
 
-confusionMatrix(confusion$predicted,confusion$vis_class)
+confusionMatrix(confusion$predicted,confusion$vis_class, mode = "prec_recall")
 
 
 
@@ -101,7 +111,7 @@ files<-list.files(".","*.jpg", full.names = T,recursive = T)
 
 
 
-setwd("~/development/fogDec//")
+setwd("~/development/fogDec/")
 
 dbConfig <- fromJSON("config.json")
 
@@ -114,24 +124,36 @@ con <- dbConnect(RPostgreSQL::PostgreSQL(),
 
 imagesRWSDayLight <- dbGetQuery(con, "SELECT images.image_id, images.filepath, images.timestamp, images.day_phase
                                 FROM images
-                                WHERE camera_id =326 AND day_phase=1;")
+                                WHERE camera_id =326 AND day_phase=1 AND timestamp<='2017-08-24 09:01:00';")
 
 
 
 test<-sapply(imagesRWSDayLight$filepath, strsplit, "HM577/")
+test<-sapply(test, "[[", 2)
 
 
+setwd("~/images/RWS/")
+
+files<-test
+
+
+cl <- makeCluster(16)
+registerDoParallel(cl)
+
+clusterEvalQ(cl, library("imager"))
 
 matRWS<-foreach(i=1:length(files), .combine = rbind) %dopar%{
   message(files[[i]])
   image<-load.image(files[[i]])
   image<-resize(image,28,28)
+  image<-blur_anisotropic(image, amplitude = 15000)
   df<-as.data.frame(image)
   v<-df$value
   #mat<-rbind(mat,v)
   v
 }
 
+stopCluster(cl)
 
 
 predictedRWS<-predict(net,matRWS)
