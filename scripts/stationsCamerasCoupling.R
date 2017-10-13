fb89a031d2007f6ad0eaf42b08d97684db4acf92
andreapago <andrea.pago@gmail.com>
2017-10-13 19:54:51 +0000

    fixed error in computing the distance due to a setkey used after and referred before setting by values copy

--- scripts/stationsCamerasCoupling.R
+++ scripts/stationsCamerasCoupling.R
@@ -7,16 +7,16 @@ library(geosphere)
 
 
 coupleCamerasAndKNMInearStations<-function(maxDistance = 5000){
-locationKNMIStations<-read.csv("inst/extdata/testStationsGood2.csv", sep = ";",header = TRUE)
-locationKNMIStations<-data.table(locationKNMIStations)
-#fill NA with a 0 coordinate NB: Bonaire station 990 is not filled
-locationKNMIStations[is.na(lon), lon:=0]
-locationKNMIStations<-locationKNMIStations[lat!=9999 & as.Date(as.character(locationKNMIStations$endDate), "%Y%m%d")==as.Date("99991231", "%Y%m%d")]
-locationKNMIStations<-locationKNMIStations[typeStation=="Luchtdruk- en weerwaarnemingen"]
-
-uniqueKNMIStations<-unique(locationKNMIStations,by=c("lat", "lon"))
-uniqueKNMIStations[,KISID:=gsub("_A_","_WAARNEEMID_", KISID)]
-stationsForDB<-uniqueKNMIStations[, !c("lat", "lon", "startDate","endDate")]
+# locationKNMIStations<-read.csv("inst/extdata/testStationsGood2.csv", sep = ";",header = TRUE)
+# locationKNMIStations<-data.table(locationKNMIStations)
+# #fill NA with a 0 coordinate NB: Bonaire station 990 is not filled
+# locationKNMIStations[is.na(lon), lon:=0]
+# locationKNMIStations<-locationKNMIStations[lat!=9999 & as.Date(as.character(locationKNMIStations$endDate), "%Y%m%d")==as.Date("99991231", "%Y%m%d")]
+# locationKNMIStations<-locationKNMIStations[typeStation=="Luchtdruk- en weerwaarnemingen"]
+# 
+# uniqueKNMIStations<-unique(locationKNMIStations,by=c("lat", "lon"))
+# uniqueKNMIStations[,KISID:=gsub("_A_","_WAARNEEMID_", KISID)]
+# stationsForDB<-uniqueKNMIStations[, !c("lat", "lon", "startDate","endDate")]
 
 
 distanceUsableSensor<-maxDistance
@@ -30,8 +30,8 @@ con <- dbConnect(RPostgreSQL::PostgreSQL(),
                  host = dbConfig[["host"]], port = 9418,
                  user = dbConfig[["user"]], password = dbConfig[["pw"]])
 
-
-camerasTable <- as.data.table(dbReadTable(con, "cameras"))
+knmiStations <- as.data.table(dbReadTable(con, "meteo_stations"))
+#camerasTable <- as.data.table(dbReadTable(con, "cameras"))
 locationsTable <- as.data.table(dbReadTable(con, "locations"))
 dbDisconnect(con)
 
@@ -39,14 +39,21 @@ dbDisconnect(con)
 locationsCameras<-locationsTable
 #locationsPortHW<-data.table(locationsPortHW)
 
-matCameras<-cbind(locationsCameras$longitude,locationsCameras$latitude)
+setkey(knmiStations,location_id)
+setkey(locationsTable,location_id)
+combinedMeteoStationsLocations<-locationsTable[knmiStations,]
+
+matCameras<-cbind(locationsTable$longitude,locationsTable$latitude)
 matCameras<-matrix(matCameras,ncol = 2)
-matSensors<-cbind(locationKNMIStations$lon,locationKNMIStations$lat)
+
+
+
+matSensors<-cbind(combinedMeteoStationsLocations$longitude,combinedMeteoStationsLocations$latitude)
 matSensors<-matrix(matSensors,ncol=2)
 distm(matSensors,matCameras)
 distances<-distm(matSensors,matCameras)
-rownames(distances)<-locationKNMIStations$KISID
-colnames(distances)<-locationsCameras$location_id
+rownames(distances)<-combinedMeteoStationsLocations$knmi_kis_id
+colnames(distances)<-locationsTable$location_id
 distances[distances>distanceUsableSensor]<-NA
 distancesDT<-data.table(distances)
 
