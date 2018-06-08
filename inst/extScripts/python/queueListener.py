0dcb9ca7f69b2191a21698fe531acfb34e27b369
andreapago <andrea.pago@gmail.com>
2018-06-08 14:05:42 +0200

    updated queue listener with filter on the day phase

--- inst/extScripts/python/queueListener.py
+++ inst/extScripts/python/queueListener.py
@@ -1,27 +1,102 @@
 import pika
 import json
 from jsonpath_rw import parse
+import ephem
+import subprocess
+import datetime
 
 def readJson(confFile):
     with open(confFile) as data_file:
         data = json.load(data_file)
     return data
 
-confFileQueue = "queueConfig.json"
+#function to compute the phase of the day (day, night, dawn, dusk)
+#based on the day of year and latitude and longitude of the location
+#an ID corresponding to the phase of the day is returned, the ID is
+# based on the mapping decided and put in the DB table day_phase
+def computeDayPhase(lon, lat, dateTime):
+    obs = ephem.Observer()
+    obs.lat = str(lat)
+    obs.lon = str(lon)
+    #changing date to midday to avoid that previous sunrise and next sunset are in the same day
+    dateToUse = dateTime.replace(hour=12, minute=00)
+    obs.date = dateToUse
+    #obs.date = "2017/5/16 12:00"
 
-dataConfQueue = readJson(confFileQueue)
-user = dataConfQueue["user"]
+    # Civil twilight uses the value -6 degrees
+    # Nautical twilight uses the value -12 degrees
+    # Astronomical twilight uses the value -18 degrees
 
-# credentials = pika.PlainCredentials(user, pwd)
-# parameters = pika.ConnectionParameters(ipAddr, port, credentials=credentials)
-# connection = pika.BlockingConnection(parameters)
-# channel = connection.channel()
-#
-# channel.queue_declare(queue='test')
+    obs.horizon = "-18"
+    try:
+        astroDawn = obs.previous_rising(ephem.Sun(), use_center=True)
+        astroDusk = obs.next_setting(ephem.Sun(), use_center=True)
+    except ephem.AlwaysUpError:
+        astroDawn = ephem.Date(dateTime - datetime.timedelta(days=1))
+        astroDusk = ephem.Date(dateTime + datetime.timedelta(days=1))
+        #print("There is no night")
+    #astroDusk = obs.next_setting(ephem.Sun(), use_center=True)
 
-confFile = "MVPCameras.json"
+    obs.horizon = "-12"
+    try:
+        nauticalDawn = obs.previous_rising(ephem.Sun(), use_center=True)
+        nauticalDusk = obs.next_setting(ephem.Sun(), use_center=True)
+    except ephem.AlwaysUpError:
+        nauticalDawn = ephem.Date(dateTime - datetime.timedelta(days=1))
+        nauticalDusk = ephem.Date(dateTime + datetime.timedelta(days=1))
+        #print("There is no astronomical dawn-dusk")
 
-#read the config file in JSON
+    obs.horizon = "-6"
+    try:
+        civilDawn = obs.previous_rising(ephem.Sun(), use_center=True)
+        civilDusk = obs.next_setting(ephem.Sun(), use_center=True)
+    except ephem.AlwaysUpError:
+        civilDawn = ephem.Date(dateTime - datetime.timedelta(days=1))
+        civilDusk = ephem.Date(dateTime + datetime.timedelta(days=1))
+        #print("There is no nautical dawn-dusk")
+
+    #for the setting used refer to:
+    #http://rhodesmill.org/pyephem/quick#transit-rising-setting
+    obs.horizon = "0"
+    try:
+        sunrise = obs.previous_rising(ephem.Sun(), use_center=True)
+        sunset = obs.next_setting(ephem.Sun(), use_center=True)
+    except ephem.AlwaysUpError:
+        sunrise = -1
+        sunset = -1
+        #print("There is no civil dawn-dusk")
+
+    dayPhaseID = -1
+
+    if(dateTime < sunrise.datetime() and dateTime >= civilDawn.datetime()):
+        #print "civil dawn"
+        dayPhaseID =10
+    if (dateTime < civilDawn.datetime() and dateTime >= nauticalDawn.datetime()):
+        #print "nautical dawn"
+        dayPhaseID = 20
+    if (dateTime < nauticalDawn.datetime() and dateTime > astroDawn.datetime()):
+        #print "astro dawn"
+        dayPhaseID = 30
+    if(dateTime <= astroDawn.datetime()):
+        #print "night"
+        dayPhaseID  = 0
+    if(dateTime >= sunrise.datetime() and dateTime <= sunset.datetime()):
+        #print "day"
+        dayPhaseID = 1
+    if (dateTime > sunset.datetime() and dateTime <= civilDusk.datetime()):
+        #print "civil dusk"
+        dayPhaseID = 11
+    if (dateTime > civilDusk.datetime() and dateTime <= nauticalDusk.datetime()):
+        #print "nautical dusk"
+        dayPhaseID = 21
+    if (dateTime > nauticalDusk.datetime() and dateTime < astroDusk.datetime()):
+        #print "astro dusk"
+        dayPhaseID = 31
+    if (dateTime >= astroDusk.datetime()):
+        #print "night"
+        dayPhaseID = 0
+    return(dayPhaseID)
+
 
 
 #extract the relevant section of JSON config file
@@ -50,6 +125,41 @@ def extractCameras(confJson):
 
 
 
+#at the moment assuming De Bilt as the location where we assess the dayphase
+#for running the model for daylight (only model at the moment) or for
+#other moment of the day (future work)
+
+lonDeBilt = 5.1809676
+latDeBilt = 52.1092717
+
+
+confFileQueue = "queueConfig.json"
+dataConfQueue = readJson(confFileQueue)
+user = dataConfQueue["user"]
+pwd = dataConfQueue["pw"]
+ipAddr = dataConfQueue["host"]
+port = dataConfQueue["port"]
+
+
+credentials = pika.PlainCredentials(user, pwd)
+parameters = pika.ConnectionParameters(ipAddr, port, credentials=credentials)
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+
+channel.queue_declare(queue='test')
+
+confFile = "MVPCameras.json"
+
+#read the config file in JSON
+
+
+
+
+
+
+
+
+
 camerasMVPConf = readJson(confFile)
 
 locationToProcess = extractLocations(camerasMVPConf)
@@ -58,6 +168,7 @@ camerasToProcess = extractCameras(camerasMVPConf)
 
 
 print("abc")
+
 
 #
 # def callback(ch, method, properties, body):
@@ -72,10 +183,34 @@ print("abc")
 
 exampleMessageBody = "/nas-research.knmi.nl/sensordata/CAMERA/RWS/A2/HM776/ID10915/201806/A2-HM776-ID10915_20180606_0801.jpg"
 
+
+
+def filterDayPhase():
+    nowUTC = datetime.datetime.utcnow()
+
+    completeDateTime = datetime.datetime.strptime(
+        str(nowUTC.year) + str(nowUTC.month) + str(nowUTC.day) + str(nowUTC.hour) + str(nowUTC.minute), "%Y%m%d%H%M")
+    dayPhaseNow = computeDayPhase(lonDeBilt, latDeBilt, completeDateTime)
+    dayPhaseNow=11
+
+    # way to call R in python subprocess.call (["/usr/bin/Rscript", "--vanilla", "/pathto/MyrScript.r"])
+    callToRScript = {1: ["/usr/bin/Rscript", "--vanilla", "/pathto/MyrScript.r"]}
+
+    result = callToRScript.get(dayPhaseNow, None)
+
+    print("pippo")
+
+    return result
+
+
 def filterMessage(message, locationToProcess, camerasToProcess):
     if any(s for s in locationToProcess if s in exampleMessageBody):
         if any(ss for ss in camerasToProcess if ss in exampleMessageBody):
-            print("ABBB")
+            return (message)
+        else:
+            return(None)
+    else:
+        return(None)
 
 #==0 and message.find(camerasToProcess)==0):
 #print("location camera should be processed")
@@ -83,4 +218,9 @@ def filterMessage(message, locationToProcess, camerasToProcess):
 
 
 
-filterMessage(exampleMessageBody, locationToProcess, camerasToProcess)
+
+message = filterMessage(exampleMessageBody, locationToProcess, camerasToProcess)
+if (message != None):
+    callParams = filterDayPhase()
+    if (callParams != None):
+        subprocess.call(["/usr/bin/Rscript", "--vanilla", "/pathto/MyrScript.r", message])
